1673904a2be93c8096848b868dfc5ddb16785fb0
platinum57 <sook8986@naver.com>
2025-08-05 15:50:20 +0900

    feat: 클리어한 보스 고정 처리 기능 추가

--- recommendation-service/src/main/java/com/happymapleday/recommendation/dto/request/BossSelection.java
+++ recommendation-service/src/main/java/com/happymapleday/recommendation/dto/request/BossSelection.java
@@ -18,6 +18,7 @@ public class BossSelection {
     @NotNull
     private Integer partySize; // 1: 솔로, 2이상: 파티
     
+    private boolean isCleared;
     // 2명 이상으로 클리어하는 보스인지 확인
     public boolean isPartyBoss() {
         return partySize >= 2;
